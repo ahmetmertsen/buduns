d7b26bbcb4d689d8bb88e96c80c8dd0aeebec123
ahmetmert <ahmetmert0505@gmail.com>
2026-03-24 20:32:03 +0300

    Backend: Follower servisde takip edilen kullanıcıya bildirim gönderme özelliği eklendi.

--- blogapp-server/Core/blogapp-server.Application/Features/Followers/Commands/Create/CreateFollowersCommandHandler.cs
+++ blogapp-server/Core/blogapp-server.Application/Features/Followers/Commands/Create/CreateFollowersCommandHandler.cs
@@ -2,7 +2,9 @@
 using blogapp_server.Application.Exceptions;
 using blogapp_server.Application.UnitOfWork;
 using blogapp_server.Domain.Entities;
+using blogapp_server.Domain.Entities.Identity;
 using MediatR;
+using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,10 +16,12 @@ namespace blogapp_server.Application.Features.Followers.Commands.Create
     public class CreateFollowersCommandHandler : IRequestHandler<CreateFollowersCommand, CreateFollowersCommandResponse>
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<User> _userManager;
 
-        public CreateFollowersCommandHandler(IUnitOfWork unitOfWork)
+        public CreateFollowersCommandHandler(IUnitOfWork unitOfWork, UserManager<User> userManager)
         {
             _unitOfWork = unitOfWork;
+            _userManager = userManager;
         }
 
         public async Task<CreateFollowersCommandResponse> Handle(CreateFollowersCommand request, CancellationToken cancellationToken)
@@ -40,6 +44,22 @@ namespace blogapp_server.Application.Features.Followers.Commands.Create
                 isActive = true,
                 isDeleted = false
             };
+
+            #region Takip edilen kişiye bildirim gönderme
+            // Takip eden kullanıcı
+            var user = await _userManager.FindByIdAsync(Convert.ToString(request.UserId));
+
+            Notification notification = new()
+            {
+                Type = "Following",
+                Message = $"{user.UserName} sizi takip etmeye başladı",
+                UserId = request.UserId,
+                CreatedAt = DateTime.UtcNow,
+                isActive = true,
+                isDeleted = false
+            };
+            await _unitOfWork.NotificationRepository.AddAsync(notification);
+            #endregion
 
 
             await _unitOfWork.FollowerRepository.AddAsync(follow);
